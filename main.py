952f5b330e8b8b4cb168e275d673208ad25b9598
Florian Weissenbacher <florian.weissenbacher2003@gmail.com>
2021-11-08 21:15:54 +0100

    branch erstellt, erster rpm Sensor Test

---
+++ main.py
@@ -0,0 +1,10 @@
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+while True: # Run forever
+    if GPIO.input(12) == GPIO.LOW:
+        print("0")
+    if GPIO.input(12) == GPIO.HIGH:
+         print("1")
