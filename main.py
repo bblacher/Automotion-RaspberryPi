d09369244f1fb2734abcdda12fa171bf5cb9b486
Benjamin Blacher <schule@benjaminblacher.com>
2021-11-15 13:54:23 +0100

    rpm Auswertung v1

--- main.py
+++ main.py
@@ -1,10 +1,51 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+import RPi.GPIO as GPIO
+import time
 
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-while True: # Run forever
-    if GPIO.input(12) == GPIO.LOW:
-        print("0")
-    if GPIO.input(12) == GPIO.HIGH:
-         print("1")
+sensor = 12
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(sensor, GPIO.IN)
+
+sample = 10
+count = 0
+
+start = 0
+end = 0
+
+def set_start():
+    global start
+    start = time.time()
+
+
+def set_end():
+    global end
+    end = time.time()
+
+
+def get_rpm(self):
+    global count
+
+    if not count:
+            set_start()
+            count = count + 1
+
+    else:
+            count = count + 1
+
+            if count == sample:
+                set_end()
+                delta = end - start
+                delta = delta / 60
+                rpm = (sample / delta / 20)
+                print(round(rpm,0))
+                count = 0
+                return 
+
+GPIO.add_event_detect(sensor,GPIO.RISING, callback=get_rpm)
+
+try:
+        while True:
+            time.sleep(0.1)
+except KeyboardInterrupt:
+            print ("   Quit")
+            GPIO.cleanup()  
