4029211041ff0639518ae4499a1e24fa56070bfd
Florian Weissenbacher <florian.weissenbacher2003@gmail.com>
2021-11-20 13:49:16 +0000

    delete main, rename test to main

--- main.py
+++ main.py
@@ -4,48 +4,22 @@ import time
 sensor = 12
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(sensor, GPIO.IN)
+GPIO.setup(sensor, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-sample = 10
 count = 0
+slots = 20 
 
-start = 0
-end = 0
+sample_time = 1
 
-def set_start():
-    global start
-    start = time.time()
-
-
-def set_end():
-    global end
-    end = time.time()
-
-
-def get_rpm(self):
+def count_rev(pin):
     global count
+    count = count + 1
 
-    if not count:
-            set_start()
-            count = count + 1
+GPIO.add_event_detect(sensor, GPIO.RISING, callback=count_rev)
 
-    else:
-            count = count + 1
+while True:
+    time.sleep(sample_time)
 
-            if count == sample:
-                set_end()
-                delta = end - start
-                delta = delta / 60
-                rpm = (sample / delta / 20)
-                print(round(rpm,0))
-                count = 0
-                return 
-
-GPIO.add_event_detect(sensor,GPIO.RISING, callback=get_rpm)
-
-try:
-        while True:
-            time.sleep(0.1)
-except KeyboardInterrupt:
-            print ("   Quit")
-            GPIO.cleanup()  
+    rpm = ((count / sample_time) * 60) / slots
+    print(rpm)
+    count = 0
