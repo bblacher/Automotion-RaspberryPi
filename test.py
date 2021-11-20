0418a252a2b140afa9244754086644c540954068
Florian Weissenbacher <florian.weissenbacher2003@gmail.com>
2021-11-20 13:26:11 +0000

    rpm Auswertung v2 in test

--- test.py
+++ test.py
@@ -7,31 +7,19 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(sensor, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 count = 0
-slots = 2
-start = 0
+slots = 20 
 
+sample_time = 1
 
+def count_rev(pin):
+    global count
+    count = count + 1
 
-def calculate(self):
-    global count, start
+GPIO.add_event_detect(sensor, GPIO.RISING, callback=count_rev)
 
-    if not count:
-        start = time.time()
-        count = count + 1
-    
-    else:
-        count = count + 1
+while True:
+    time.sleep(sample_time)
 
-        if time.time() - start <= 1:
-            rpm = (count * 60 / slots)
-            print(rpm)
-            count = 0
-            return
-
-GPIO.add_event_detect(sensor,GPIO.RISING, callback=calculate)
-
-try:
-    while True:
-        time.sleep(0.1)
-except KeyboardInterrupt:
-        GPIO.cleanup()
+    rpm = ((count / sample_time) * 60) / slots
+    print(rpm)
+    count = 0
