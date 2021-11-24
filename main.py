2e6d6b36dc5731b66764783ddfdb2c5d6463aa74
Benjamin Blacher <schule@benjaminblacher.com>
2021-11-24 16:17:54 +0100

    Implement SD-Card logging

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import FaBo9Axis_MPU9250
 import time
 import sys
 import math
+import os
 from datetime import datetime
+
+if not os.path.exists('data'):
+    os.makedirs('data')
 
 now = datetime.now()
 
-file = open("./data/data.txt",'w') #change mode to 'a' whan merging, has to be 'a' when shipping
+file = open("./data/data.txt",'w+') #change mode to 'a+' when merging, has to be 'a+' when shipping
 
 mpu9250 = FaBo9Axis_MPU9250.MPU9250()
 
@@ -56,23 +60,37 @@ while 1:
     if angz < 0:
         angz = 360 + angz
 
-    print(" ax = " , ax_cal)
-    print(" ay = " , ay_cal)
-    print(" az = " , az_cal)
+    print("ax = " + str(ax_cal))
+    print("ay = " + str(ay_cal))
+    print("az = " + str(az_cal))
 
-    print(" gx = " , gx_cal)
-    print(" gy = " , gy_cal)
-    print(" gz = " , gz_cal)
+    print("gx = " + str(gx_cal))
+    print("gy = " + str(gy_cal))
+    print("gz = " + str(gz_cal))
 
-    print(" mx = " , mx_cal)
-    print(" my = " , my_cal)
-    print(" mz = " , mz_cal)
+    print("mx = " + str(mx_cal))
+    print("my = " + str(my_cal))
+    print("mz = " + str(mz_cal))
 
-    print(" Temp = " , temp)
+    print("Temp = " + str(temp))
 
     print("angx = " + str(angx))
     print("angy = " + str(angy))
     print("angz = " + str(angz))
 
-    file.write(str(now)+"\n")
+    file.write(str(now)+",")
+    file.write(str(ax_cal)+",")
+    file.write(str(ay_cal)+",")
+    file.write(str(az_cal)+",")
+    file.write(str(gx_cal)+",")
+    file.write(str(gy_cal)+",")
+    file.write(str(gz_cal)+",")
+    file.write(str(mx_cal)+",")
+    file.write(str(my_cal)+",")
+    file.write(str(mz_cal)+",")
+    file.write(str(temp)+",")
+    file.write(str(angx)+",")
+    file.write(str(angy)+",")
+    file.write(str(angz)+",")
+    file.write("\n")
     time.sleep(0.2)
