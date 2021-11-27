c28bcfdb2b64b4180abe532781624e7e64b20da4
Florian Weissenbacher <florian.weissenbacher2003@gmail.com>
2021-11-27 15:08:41 +0100

    gps.py -> main.py

--- main.py
+++ main.py
@@ -1,81 +1,17 @@
-import RPi.GPIO as GPIO
+import serial
 import time
-from datetime import datetime
-import sys
-import math
-
-sensor_rear_L = 11
-sensor_rear_R = 12
-sensor_front_L = 13
-sensor_front_R = 15
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(sensor_rear_L, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(sensor_rear_R, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(sensor_front_L, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(sensor_front_R, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-count_rear_L = 0
-count_rear_R = 0
-count_front_L = 0
-count_front_R = 0
-
-slots_rear = 20 
-slots_front = 2
-d_wheel = 0.14
-sample_time = 1
-
-def counter_rear_L(pin):
-    global count_rear_L
-    count_rear_L = count_rear_L + 1
-
-def counter_rear_R(pin):
-    global count_rear_R
-    count_rear_R = count_rear_R + 1
-
-def counter_front_L(pin):
-    global count_front_L
-    count_front_L = count_front_L + 1
-
-def counter_front_R(pin):
-    global count_front_R
-    count_front_R = count_front_R + 1
-
-GPIO.add_event_detect(sensor_rear_L, GPIO.RISING, callback=counter_rear_L) 
-GPIO.add_event_detect(sensor_rear_R, GPIO.RISING, callback=counter_rear_R)  
-GPIO.add_event_detect(sensor_front_L, GPIO.RISING, callback=counter_front_L)
-GPIO.add_event_detect(sensor_front_R, GPIO.RISING, callback=counter_front_R)
-
-now = datetime.now()
-
-file = open("data.txt",'a')
-file.write("NewData "+str(now)+"\n")
+import string
+import pynmea2
 
 while True:
-    time.sleep(sample_time)
+	port="/dev/ttyAMA0"
+	ser=serial.Serial(port, baudrate=9600, timeout=0.5)
+	dataout = pynmea2.NMEAStreamReader()
+	newdata=ser.readline()
 
-        
-    rpm_rear_L = ((float(count_rear_L) / slots_rear) / sample_time) * 60
-    rpm_rear_R = ((float(count_rear_R) / slots_rear) / sample_time) * 60
-    rpm_front_L = ((float(count_front_L) / slots_front) / sample_time) * 60
-    rpm_front_R = ((float(count_front_R) / slots_front) / sample_time) * 60
-    vel_ms = d_wheel * math.pi * (float((rpm_front_L + rpm_front_R) / 2) / 60)
-
-    print(rpm_rear_L)
-    print(rpm_rear_R)
-    print(rpm_front_L)
-    print(rpm_front_R)
-    print(vel_ms)
-    print("---------")
-
-    now = datetime.now()
-
-    file.write(str(rpm_rear_L)+","+str(rpm_rear_R)+","+str(rpm_front_L)+","+str(rpm_front_R)+","+str(vel_ms)+",")
-    file.write(str(now)+"\n")
-
-    
-    count_rear_L = 0
-    count_rear_R = 0
-    count_front_L = 0
-    count_front_R = 0
-
+	if newdata[0:6] == "$GPRMC":
+		newmsg=pynmea2.parse(newdata)
+		lat=newmsg.latitude
+		lng=newmsg.longitude
+		gps = "Latitude=" + str(lat) + "and Longitude=" + str(lng)
+		print(gps)
