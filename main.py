1f4b0fc946b09fdfab9c63a40196a4b847b97a5d
Benjamin Blacher <schule@benjaminblacher.com>
2021-11-29 12:55:49 +0100

    Add comments for usb-automount

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 # Run this script as root to get it working
 
-import os
-import time
-import shutil
+import os   #Used to check if a drive is mounted
+import time #Used for delays
+import shutil   #Used for file operations
 
 while 1:
-    ismounted = os.path.ismount("/media/usb0")
-    print("Device mounted: " + str(ismounted))
-    if ismounted:
+    ismounted = os.path.ismount("/media/usb0")  #check if a drive is mounted
+    print("Device mounted: " + str(ismounted))  #output drive mount status
+    if ismounted:                               #if a drive is mounted, copy the datafile to it
         shutil.copy("./data/data.txt","/media/usb0")
-        print("copied!")
-        while ismounted:
+        print("copied!")                        #Confirmation that the file was copied successfully
+        while ismounted:                        #while the device is mounted, tell the user to unplug it
             ismounted = os.path.ismount("/media/usb0")
-            print("remove drive!")
-            time.sleep(1)
-    time.sleep(1)
+            print("remove drive!")      
+            time.sleep(1)                       #1 Second delay
+    time.sleep(1)                               #1 Second delay
