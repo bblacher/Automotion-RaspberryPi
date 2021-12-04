84c534d2b4474abfe82c0db5cf0c975acacaabf1
Benjamin Blacher <schule@benjaminblacher.com>
2021-12-04 20:20:31 +0100

    Acceleration using sensorfusion added (not working properly)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import smbus
+import math
 
 from imusensor.MPU9250 import MPU9250
 from imusensor.filters import madgwick
@@ -27,6 +28,9 @@ imu.loadCalibDataFromFile("/home/pi/Diplomarbeit/RC-Car/config/Calib.json")
 currTime = time.time()
 
 print_count = 0
+g = 10
+
+file.write("NewData "+str(now)+"\n")
 
 while 1:
     imu.readSensor()
@@ -38,7 +42,7 @@ while 1:
         sensorfusion.updateRollPitchYaw(imu.AccelVals[0], imu.AccelVals[1], imu.AccelVals[2], imu.GyroVals[0], \
 									imu.GyroVals[1], imu.GyroVals[2], imu.MagVals[0], imu.MagVals[1], imu.MagVals[2], dt)
 
-    if print_count == 2:
+    if print_count == 10:
 
         now = datetime.now()
         roll = sensorfusion.roll
@@ -46,25 +50,46 @@ while 1:
         yaw = sensorfusion.yaw
         temp = imu.Temp
 
-        if roll < 0:
-            roll = 360 + roll
-
-        if pitch < 0:
-            pitch = 360 + pitch
-
-        if yaw < 0:
-            yaw = 360 + yaw
-
+        file.write(str(now)+",")
         print("roll: " + str(roll))
         print("pitch: " + str(pitch))
         print("yaw: " + str(yaw))
-        print("Temp: " + str(temp))
 
-        file.write(str(now)+",")
         file.write(str(roll)+",")
         file.write(str(pitch)+",")
         file.write(str(yaw)+",")
-        file.write(str(temp)+",")
+        a = math.radians(roll - 90)
+        b = math.radians(pitch - 90)
+        c = math.radians(yaw - 90)
+       
+        if a < math.pi * -1:
+            a = a + 2 * math.pi
+        if b < math.pi * -1:
+            b = b + 2 * math.pi
+        if c < math.pi * -1:
+            c = c + 2 * math.pi
+
+        if math.atan(c/b) < 0:
+            ax = imu.AccelVals[0] + g*math.tan(c)/math.sqrt((math.tan(c)** 2)/(math.cos(b) ** 2) +1)
+        elif math.atan(c/b) > 0:
+            ax = imu.AccelVals[0] - g*math.tan(c)/math.sqrt((math.tan(c)** 2)/(math.cos(b) ** 2) +1)
+
+        if math.atan(c/a) < 0:
+            ay = imu.AccelVals[1] + g/math.sqrt(math.tan(c) ** 2 + math.tan(a) ** 2 + 1 )
+        elif math.atan(c/a) > 0:
+            ay = imu.AccelVals[1] - g/math.sqrt(math.tan(c) ** 2 + math.tan(a) ** 2 + 1 )
+        
+        if math.atan(a/b) < 0:
+            az = imu.AccelVals[2] + g/math.sqrt((1 / math.tan(a)) ** 2 + (1 / math.tan(b)) ** 2 + 1 )
+        elif math.atan(a/b) > 0:
+            az = imu.AccelVals[2] - g/math.sqrt((1 / math.tan(a)) ** 2 + (1 / math.tan(b)) ** 2 + 1 )
+
+        print("Ax " + str(ax))
+        print("Ay " + str(ay))
+        print("Az " + str(az))
+        print("Temp: " + str(temp))
+
+        file.write(str(temp))
         file.write("\n")
 
         print_count = 0
