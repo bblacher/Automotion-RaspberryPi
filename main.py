ea1c742c194b3aac6218bc558d7794da602ceb1e
Benjamin Blacher <schule@benjaminblacher.com>
2022-01-03 15:09:29 +0100

    Implement button mode switching

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ import time         # Used for delays
 import smbus        # Used for sensor access
 import math         # Used for math operations
 import shutil       # Used for file operations
+import RPi.GPIO as GPIO  # Needed for GPIO actions (eg. Buttons)
 
 from multiprocessing import Process     # Used for multiprocessing
 from imusensor.MPU9250 import MPU9250   # Used for getting MPU9250 readings
 from imusensor.filters import madgwick  # Used for the madgwick filter
 from datetime import datetime           # Used for the madgwick filter timing
 
+
 # Function definitions:
 def usb_automount():
+    # TODO this isn't working anymore
     done = False    # init done as false
-    timeout = 15    # set timeout to 15 seconds
-    while not done and timeout > 1:
+    while not done and not collecting_data:
         ismounted = os.path.ismount("/media/usb0")              # check if a drive is mounted
         print("Device mounted: " + str(ismounted))              # output drive mount status
         if ismounted:                                           # if a drive is mounted, copy the datafile to it
@@ -29,7 +31,6 @@ def usb_automount():
                 print("remove drive!")                          # Tell the user to remove the drive
                 done = True                                     # Break the automount loop
                 time.sleep(1)                                   # 1-Second delay
-        timeout -= 1                                            # Decrease the timeout timer
         time.sleep(1)                                           # 1-Second delay
 
 
@@ -52,11 +53,13 @@ def sensor_fusion():
 
 
 def get_gps():
-    newdata = ser.readline()                                    # get new data
-    newmsg = pynmea2.parse(newdata)                             # parse new data
-    lat = newmsg.latitude                                       # save latitude
-    lng = newmsg.longitude                                      # save longitude
-    gps_data = str(lat) + "," + str(lng)                        # save gps data as string
+    # newdata = ser.readline()                                    # get new data
+    # newmsg = pynmea2.parse(newdata)                             # parse new data
+    # lat = newmsg.latitude                                       # save latitude
+    # lng = newmsg.longitude                                      # save longitude
+    # gps_data = str(lat) + "," + str(lng)                        # save gps data as string
+    # TODO Fix this
+    gps_data = "0,0"
     return gps_data                                             # return gps data
 
 
@@ -84,86 +87,103 @@ def write_data(u_now, u_roll, u_pitch, u_yaw, u_ax, u_ay, u_az, u_temp, u_gps): 
     file.write("\n")  # write newline
 
 
+def start_stop(pin):
+    global collecting_data
+    collecting_data = not collecting_data
+    if not collecting_data:
+        file.close()
+
+
 if not os.path.exists('data'):  # If the data path doesn't exit, create it
     os.makedirs('data')
 
-now = str(datetime.now())                           # get datetime for the file name
-now = now.replace(' ', '_')                         # replace blank space with underline for the file name
-now = now.replace(':', '_')                         # replace colon with underline for the file name
-now = now.replace('.', '_')                         # replace dot with underline for the file name
-file = open("./data/" + now + ".txt", 'w')          # create and open a new datafile
 
-sensorfusion = madgwick.Madgwick(0.5)               # set Madgwick as the sensorfusion-algorythm
-address = 0x68                                      # MPU9250 I2C-Address
-bus = smbus.SMBus(1)                                # smbus for the imu
-imu = MPU9250.MPU9250(bus, address)                 # set MPU9250 as the selected IMU
 try:                                                # Error handling for the IMU
+    sensorfusion = madgwick.Madgwick(0.5)           # set Madgwick as the sensorfusion-algorythm
+    address = 0x68                                  # MPU9250 I2C-Address
+    bus = smbus.SMBus(1)                            # smbus for the imu
+    imu = MPU9250.MPU9250(bus, address)             # set MPU9250 as the selected IMU
     imu.begin()                                     # begin IMU readings
     imu.loadCalibDataFromFile("./config/Calib.json")  # load calibration data
     imuerror = False                                # Set imuerror false for later use
     process_sensorfusion = Process(target=sensor_fusion)  # create thread for the sensorfusion
-    process_sensorfusion.start()                     #start the thread for the sensorfusion
+    process_sensorfusion.start()                    #start the thread for the sensorfusion
 except:                                             # Except-Statement for imuerror
     print("MPU 9250: Error! (Not connected?)")      # Write error message
     imuerror = True                                 # Set imuerror true for later use
 
+collecting_data = False                             # init collecting_data
 g = 10                                              # set g as 10
-
+modeswitch = 40                                     # set the modeswitch Button to PIN 40
 port = "/dev/ttyAMA0"                               # define UART device
 ser = serial.Serial(port, baudrate=9600)            # set serial communication options
 
-file.write("datetime,roll,pitch,yaw,ax,ay,az,Temp,lat,lng\n")    # write the data legend into a new line
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(modeswitch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.add_event_detect(modeswitch, GPIO.FALLING, callback=start_stop, bouncetime=1000)
 
 while 1:
-        now = datetime.now()                    # get datetime
-        if not imuerror:
-            roll = sensorfusion.roll                # get roll
-            pitch = sensorfusion.pitch              # get pitch
-            yaw = sensorfusion.yaw                  # get yaw
-            temp = imu.Temp                         # get temp
+    if not collecting_data:
+        usb_automount()
 
-            a = math.radians(roll - 90)             # flip the roll data by -90 degrees and save it into a
-            b = math.radians(pitch + 90)            # flip the pitch data by 90 degrees and save it into a
+    elif collecting_data:
+        now = str(datetime.now())  # get datetime for the file name
+        now = now.replace(' ', '_')  # replace blank space with underline for the file name
+        now = now.replace(':', '_')  # replace colon with underline for the file name
+        now = now.replace('.', '_')  # replace dot with underline for the file name
+        file = open("./data/" + now + ".txt", 'w')  # create and open a new datafile
+        file.write("datetime,roll,pitch,yaw,ax,ay,az,Temp,lat,lng\n")  # write the data legend into a new line
+        while collecting_data:
+            now = datetime.now()                    # get datetime
+            if not imuerror:
+                roll = sensorfusion.roll                # get roll
+                pitch = sensorfusion.pitch              # get pitch
+                yaw = sensorfusion.yaw                  # get yaw
+                temp = imu.Temp                         # get temp
 
-            flipa = math.radians(roll - 180)        # flip the roll data by -180 degrees and save it into flipa
+                a = math.radians(roll - 90)             # flip the roll data by -90 degrees and save it into a
+                b = math.radians(pitch + 90)            # flip the pitch data by 90 degrees and save it into a
 
-            if a < math.pi * -1:                    # if a is now less than -pi
-                a = a + 2 * math.pi                 # flip it by 2pi
+                flipa = math.radians(roll - 180)        # flip the roll data by -180 degrees and save it into flipa
 
-            if b > math.pi:                         # if a is now more than pi
-                b = b - 2 * math.pi                 # flip it by -2pi
+                if a < math.pi * -1:                    # if a is now less than -pi
+                    a = a + 2 * math.pi                 # flip it by 2pi
 
-            if flipa < math.pi * -1:                # if flip a is now less than -pi
-                flipa = flipa + 2 * math.pi         # flip it by 2pi
+                if b > math.pi:                         # if a is now more than pi
+                    b = b - 2 * math.pi                 # flip it by -2pi
 
-            xoffs = math.sqrt(((g * math.tan(math.radians(90))) / math.sqrt((math.tan(math.radians(90)) ** 2) / (math.cos(b) ** 2) + 1)) ** 2)  # Offset in x
-            yoffs = g / math.sqrt((math.tan(0) ** 2) + (math.tan(a) ** 2) + 1)              # Offset in y
-            zoffs = g / math.sqrt(((1 / math.tan(a)) ** 2) + ((1 / math.tan(b)) ** 2) + 1)  # Offset in z
+                if flipa < math.pi * -1:                # if flip a is now less than -pi
+                    flipa = flipa + 2 * math.pi         # flip it by 2pi
 
-            if pitch < 0:                       # check if x-Offset should be subtracted
-                ax = imu.AccelVals[0] - xoffs   # subtract x-Offset
-            elif pitch > 0:                     # check if x-Offset should be added
-                ax = imu.AccelVals[0] + xoffs   # add x-Offset
+                xoffs = math.sqrt(((g * math.tan(math.radians(90))) / math.sqrt((math.tan(math.radians(90)) ** 2) / (math.cos(b) ** 2) + 1)) ** 2)  # Offset in x
+                yoffs = g / math.sqrt((math.tan(0) ** 2) + (math.tan(a) ** 2) + 1)              # Offset in y
+                zoffs = g / math.sqrt(((1 / math.tan(a)) ** 2) + ((1 / math.tan(b)) ** 2) + 1)  # Offset in z
 
-            if flipa < 0:                       # check if y-Offset should be subtracted
-                ay = imu.AccelVals[1] - yoffs   # subtract y-Offset
-            elif flipa > 0:                     # check if y-Offset should be added
-                ay = imu.AccelVals[1] + yoffs   # add y-Offset
+                if pitch < 0:                       # check if x-Offset should be subtracted
+                    ax = imu.AccelVals[0] - xoffs   # subtract x-Offset
+                elif pitch > 0:                     # check if x-Offset should be added
+                    ax = imu.AccelVals[0] + xoffs   # add x-Offset
 
-            if a * b < 0:                       # check if z-Offset should be subtracted
-                az = imu.AccelVals[2] - zoffs   # subtract z-Offset
-            elif a * b > 0:                     # check if z-Offset should be added
-                az = imu.AccelVals[2] + zoffs   # add z-Offset
-        elif imuerror:
-            roll = "error"  # write error into imusensor values
-            pitch = "error"  # write error into imusensor values
-            yaw = "error"  # write error into imusensor values
-            ax = "error"  # write error into imusensor values
-            ay = "error"  # write error into imusensor values
-            az = "error"  # write error into imusensor values
-            temp = "error"  # write error into imusensor values
+                if flipa < 0:                       # check if y-Offset should be subtracted
+                    ay = imu.AccelVals[1] - yoffs   # subtract y-Offset
+                elif flipa > 0:                     # check if y-Offset should be added
+                    ay = imu.AccelVals[1] + yoffs   # add y-Offset
 
-        gps = get_gps()                                             # get GPS data
-        print_data(roll, pitch, yaw, ax, ay, az, temp, gps)         # print the data (meant for debugging purposes)
-        write_data(now, roll, pitch, yaw, ax, ay, az, temp, gps)    # write the data to the internal sd card
-        time.sleep(1)
+                if a * b < 0:                       # check if z-Offset should be subtracted
+                    az = imu.AccelVals[2] - zoffs   # subtract z-Offset
+                elif a * b > 0:                     # check if z-Offset should be added
+                    az = imu.AccelVals[2] + zoffs   # add z-Offset
+
+            elif imuerror:
+                roll = "error"  # write error into imusensor values
+                pitch = "error"  # write error into imusensor values
+                yaw = "error"  # write error into imusensor values
+                ax = "error"  # write error into imusensor values
+                ay = "error"  # write error into imusensor values
+                az = "error"  # write error into imusensor values
+                temp = "error"  # write error into imusensor values
+
+            gps = get_gps()                                             # get GPS data
+            print_data(roll, pitch, yaw, ax, ay, az, temp, gps)         # print the data (meant for debugging purposes)
+            write_data(now, roll, pitch, yaw, ax, ay, az, temp, gps)    # write the data to the internal sd card
+            time.sleep(1)
