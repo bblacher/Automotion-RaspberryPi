3f9ab2e60d6c54861c184609751da7d06d62297e
Benjamin Blacher <schule@benjaminblacher.com>
2022-01-04 13:42:48 +0100

    prepare merge

--- main.py
+++ main.py
@@ -1,8 +1,45 @@
 import RPi.GPIO as GPIO
 import time
-from datetime import datetime
-import sys
 import math
+from multiprocessing import Process, Queue     # Used for multiprocessing
+
+def counter_rear_l(pin):
+    global count_rear_l
+    count_rear_l = count_rear_l + 1
+
+
+def counter_rear_r(pin):
+    global count_rear_r
+    count_rear_r = count_rear_r + 1
+
+
+def counter_front_l(pin):
+    global count_front_l
+    count_front_l = count_front_l + 1
+
+
+def counter_front_r(pin):
+    global count_front_r
+    count_front_r = count_front_r + 1
+
+def get_rpm():
+    d_wheel = 0.14
+    sample_time = 1
+    slots_rear = 20
+    slots_front = 20
+    while 1:
+        time.sleep(sample_time)
+        rpm_rear_l = ((float(count_rear_l) / slots_rear) / sample_time) * 60
+        rpm_rear_r = ((float(count_rear_r) / slots_rear) / sample_time) * 60
+        rpm_front_l = ((float(count_front_l) / slots_front) / sample_time) * 60
+        rpm_front_r = ((float(count_front_r) / slots_front) / sample_time) * 60
+        vel_ms = d_wheel * math.pi * (float((rpm_front_l + rpm_front_r) / 2) / 60)
+        count_rear_l = 0
+        count_rear_r = 0
+        count_front_l = 0
+        count_front_r = 0
+        rpm_queue.put(str(rpm_rear_l)+","+str(rpm_rear_r)+","+str(rpm_front_l)+","+str(rpm_front_r)+","+str(vel_ms))
+
 
 sensor_rear_L = 11
 sensor_rear_R = 12
@@ -14,68 +51,15 @@ GPIO.setup(sensor_rear_L, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(sensor_rear_R, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(sensor_front_L, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(sensor_front_R, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.add_event_detect(sensor_rear_L, GPIO.RISING, callback=counter_rear_l)
+GPIO.add_event_detect(sensor_rear_R, GPIO.RISING, callback=counter_rear_r)
+GPIO.add_event_detect(sensor_front_L, GPIO.RISING, callback=counter_front_l)
+GPIO.add_event_detect(sensor_front_R, GPIO.RISING, callback=counter_front_r)
 
-count_rear_L = 0
-count_rear_R = 0
-count_front_L = 0
-count_front_R = 0
+rpm_queue = Queue()
+rpm_process = Process(target=get_rpm)
+rpm_process.start()
 
-slots_rear = 20 
-slots_front = 20
-d_wheel = 0.14
-sample_time = 1
 
-def counter_rear_L(pin):
-    global count_rear_L
-    count_rear_L = count_rear_L + 1
 
-def counter_rear_R(pin):
-    global count_rear_R
-    count_rear_R = count_rear_R + 1
-
-def counter_front_L(pin):
-    global count_front_L
-    count_front_L = count_front_L + 1
-
-def counter_front_R(pin):
-    global count_front_R
-    count_front_R = count_front_R + 1
-
-GPIO.add_event_detect(sensor_rear_L, GPIO.RISING, callback=counter_rear_L) 
-GPIO.add_event_detect(sensor_rear_R, GPIO.RISING, callback=counter_rear_R)  
-GPIO.add_event_detect(sensor_front_L, GPIO.RISING, callback=counter_front_L)
-GPIO.add_event_detect(sensor_front_R, GPIO.RISING, callback=counter_front_R)
-
-now = datetime.now()
-
-file = open("data.txt",'a')
-file.write("NewData "+str(now)+"\n")
-
-while True:
-    time.sleep(sample_time)
-
-        
-    rpm_rear_L = ((float(count_rear_L) / slots_rear) / sample_time) * 60
-    rpm_rear_R = ((float(count_rear_R) / slots_rear) / sample_time) * 60
-    rpm_front_L = ((float(count_front_L) / slots_front) / sample_time) * 60
-    rpm_front_R = ((float(count_front_R) / slots_front) / sample_time) * 60
-    vel_ms = d_wheel * math.pi * (float((rpm_front_L + rpm_front_R) / 2) / 60)
-
-    print(rpm_rear_L)
-    print(rpm_rear_R)
-    print(rpm_front_L)
-    print(rpm_front_R)
-    print(vel_ms)
-    print("---------")
-
-    now = datetime.now()
-
-    file.write(str(rpm_rear_L)+","+str(rpm_rear_R)+","+str(rpm_front_L)+","+str(rpm_front_R)+","+str(vel_ms)+",")
-    file.write(str(now)+"\n")
-
-    
-    count_rear_L = 0
-    count_rear_R = 0
-    count_front_L = 0
-    count_front_R = 0
 
